589f815a6b5261246f140968d060409faf9236bd
SATHIYENDRAN05 <sathiyendran.developer05@gmail.com>
2026-05-22 14:18:06 +0530

    change_btn_color

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <button>
+      <button style={{background:
+        "red"
+      }}>
         Click me!
       </button>    
       <h1>AI-Cooking-Website</h1>
